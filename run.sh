6bbd7e53a2b711d7df7d3bf7d2ce29ccb7e346b7
ywwzwb <ywwzwb@gmail.com>
2016-11-29 14:00:10 +0800

    使用了镜像地址, 使 redis server 运行在主线程中, 防止 dsm 中的docker 进程结束

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ ls ${BASE_DIR}/ >> /tmp/error 2>&1
                                      
 echo $PATH >> /tmp/error           
 echo $LD_LIBRARY_PATH >> /tmp/error                                            
-sudo /etc/init.d/redis-server restart
-sudo redis-server >> /tmp/error 2>&1 &                                              
+sudo /etc/init.d/redis-server restart                                    
 sudo python3.4 ${BASE_DIR}/crysadm/crysadm_helper.py >> /tmp/error 2>&1 &
 sudo python3.4 ${BASE_DIR}/crysadm/crysadm.py >> /tmp/error 2>&1 & 
+sudo redis-server >> /tmp/error 2>&1           
