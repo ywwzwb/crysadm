a5a0c5b8420eb6e6e88a2eca9e0afe5741f0abcc
ywwzwb <ywwzwb@gmail.com>
2016-11-29 14:36:27 +0800

    修改为安静模式, 合并 pip 多个包为一条语句

--- setup.sh
+++ setup.sh
@@ -5,24 +5,21 @@
 
 #更新源
 
-sudo apt-get update 
+sudo apt-get -q update 
 
 #安装python3.4
 
-sudo apt-get install -y python3.4 python3.4-dev 
+sudo apt-get -qy install python3.4 python3.4-dev 
 
 #安装pip，确保本脚本和get-pip.py 文件在一个文件夹
 BASE_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 sudo chmod +x ${BASE_DIR}/get-pip.py
-sudo python3.4 ${BASE_DIR}/get-pip.py -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+sudo python3.4 ${BASE_DIR}/get-pip.py -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com -q
 
-sudo pip3.4 install redis -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-sudo pip3.4 install requests -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-sudo pip3.4 install flask -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
-sudo pip3.4 install flask-mail -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+sudo pip3.4 install redis requests flask flask-mail -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com -q
 
 #安装redis-server
-sudo apt-get install -y redis-server 
+sudo apt-get -qy install redis-server 
 
 
 #运行云监工
